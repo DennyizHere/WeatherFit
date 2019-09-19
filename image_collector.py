0fa3acad4fde6bce7edf0b3c851805f510207f9d
Unknown <dennycao202@yahoo.com>
2019-09-19 13:38:07 -0700

    Added more subreddits and also made a method that initializes directories and adds 100 posts

--- image_collector.py
+++ image_collector.py
@@ -13,8 +13,6 @@ def image_collector(subreddits):
         final_url = url + item + url_end
         r = requests.get(final_url,  headers = {'User-agent': 'your bot 0.1'})
         subreddit = r.json()
-        if not os.path.exists(item):
-            os.makedirs(item)
 
         for i in range(len(subreddit['data']['children'])):
             domain = subreddit['data']['children'][i]['data']['domain']
@@ -31,10 +29,42 @@ def image_collector(subreddits):
             urllib.request.urlretrieve(post_url, folder_location + image_name)
             print ("Downloading " + subreddit['data']['children'][i]['data']['title'] + " in r/" + item)
 
+def initial(subreddit):
+    url = "https://www.reddit.com/r/"
+    url_end = "/top/.json?t=year&count=100"
+
+    final_url = url + subreddit + url_end
+    r = requests.get(final_url, headers={'User-agent': 'your bot 0.1'})
+    posts = r.json()
+
+    for i in range(len(posts['data']['children'])):
+        domain = posts['data']['children'][i]['data']['domain']
+        if domain != "i.redd.it" and domain != "i.imgur.com" and domain != "m.imgur.com":
+            continue
+        post_url = posts['data']['children'][i]['data']['url']
+
+        if "imgur" in post_url:
+            if post_url[-4:] != ".jpg":
+                post_url = post_url + ".jpg"
+
+        folder_location = subreddit + "/"
+        image_name = os.path.basename(post_url)
+        urllib.request.urlretrieve(post_url, folder_location + image_name)
+        print("Downloading " + posts['data']['children'][i]['data']['title'] + " in r/" + subreddit)
+
+def dir_check(subreddits):
+    for item in subreddits:
+        if not os.path.exists(item):
+            print ("Creating " + item + " directory")
+            os.makedirs(item)
+            initial(item)
+
 def main():
 
     with open(os.path.join(sys.path[0], "subreddit_list.txt"), "r") as f:
         subreddits = f.read().splitlines()
+
+    dir_check(subreddits)
 
     print (subreddits)
     image_collector(subreddits)
